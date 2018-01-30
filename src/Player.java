26ead5f2832800548e19781c187a689796140899
zerostarm <zerostarm@users.noreply.github.com>
2018-01-30 13:29:42 -0500

    player update
    added a take turn to player

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,25 @@
 
 /**
- * This Class is the class that makes a player. 
+ * This Class is the class that makes a player.
  *
- * @author Stephen, Ryan, and Chris
- *         Created Jan 29, 2018.
+ * @author Stephen, Ryan, and Chris Created Jan 29, 2018.
  */
 public class Player {
-	
-	Player() {
-		
-	} 
-		
+	private int team = 0;
+	private String teamstr = "";
+
+	Player(int t) {// Sets up a player with a team.
+		team = t;
+		if (t == 1)
+			teamstr = "one";
+		if (t == 2)
+			teamstr = "two";
+	}
+
+	public void TakeTurn() {// Takes a player's turn.
+		System.out.println("It is player " + teamstr + "'s turn.");
+		System.out.println("Please");
+
+	}
 
 }
