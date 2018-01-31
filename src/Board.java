947521b677c00868d757add2cb8c87c1b6a34ae8
zerostarm <zerostarm@users.noreply.github.com>
2018-01-30 19:25:28 -0500

    MOVED FILES
    I had to move files into the bin anbd source folder so that I could call the files from the player class because Eclipse needs them in the same folder to allow that.
    
    Also did some adiitons to the player, and tictactoe files

---
+++ src/Board.java
@@ -0,0 +1,48 @@
+
+/**
+ * Board
+ *
+ * @author Ryan Druffel
+ * @version 0.0.0.1
+ */
+public class Board {
+	private int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+	/*
+	 * 0-Empty space 1-Represents X 2-Represents O
+	 */
+	Board() {
+		
+	}
+
+	public int[][] getBoard() { // Method so that the main method may retrieve the state of the board
+		return board;
+	}
+
+	public boolean checkVictory() {
+		for (int i = 0; i < board.length; i++) {// check horizontal victory
+
+		}
+		for (int i = 0; i < board[0].length; i++) {// check vertical victory
+
+		}
+		return false;
+	}
+
+	public void makeMove(int x, int y, int team) {
+		if ((team == 1 || team == 2) && board[y][x] == 0)
+			board[y][x] = team;
+	}
+
+	public void resetBoard() {
+		for (int x = 0; x < board.length; x++) {
+			for (int y = 0; y < board[x].length; y++) {
+				board[y][x] = 0;
+			}
+		}
+	}
+	
+	public toString() {// This needs to display the board IDK how. Graphics? Special Print way?  
+		
+	}
+}
--- src/Player.java
+++ src/Player.java
@@ -10,20 +10,24 @@ public class Player {
 	private int team = 0;
 	private String teamstr = "";
 	private Scanner scan = new Scanner(System.in);
+	private Board board = new Board();
 
-	Player(int t) {// Sets up a player with a team.
+	Player(int t, Board board1) {// Sets up a player with a team.
 		team = t;
 		if (t == 1)
 			teamstr = "one";
 		if (t == 2)
 			teamstr = "two";
+		board = board1;
 	}
 
 	public void TakeTurn() {// Takes a player's turn.
 		System.out.println("It is player " + teamstr + "'s turn.");
 		System.out.println("Please input board coordinates as integers, x first then y");
-		int x = scan.nextInt() -1 ;
+		int x = scan.nextInt() - 1;
+		int y = scan.nextInt() - 1;
 		
+		board.makeMove(x, y, team);
 	}
 
 }
