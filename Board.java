4c4cceb68c94fc6bb08a0598388d451b400c7b4b
Roudolpho <roudolphigalous@yahoo.com>
2018-01-31 12:51:51 -0500

    Finish Board and Extending Classes

--- Board.java
+++ Board.java
@@ -1,4 +1,4 @@
-package src;
+ 
 
 
 /**
@@ -8,43 +8,47 @@ package src;
  * @version 0.0.0.1
  */
 public class Board {
-	private int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+    private int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-	/*
-	 * 0-Empty space 1-Represents X 2-Represents O
-	 */
-	Board() {
-		
-	}
+    /*
+     * 0-Empty space 1-Represents X 2-Represents O
+     */
+    Board() {
+        
+    }
 
-	public int[][] getBoard() { // Method so that the main method may retrieve the state of the board
-		return board;
-	}
+    public int[][] getBoard() { // Method so that the main method may retrieve the state of the board
+        return board;
+    }
 
-	public boolean checkVictory() {
-		for (int i = 0; i < board.length; i++) {// check horizontal victory
+    public boolean checkVictory() {
+        for (int i = 0; i < board.length; i++) {// check horizontal victory
+            if(board[i][0] == board[i][1] && board[i][0] == board[i][2])
+                return true;
+        }
+        for (int i = 0; i < board[0].length; i++) {// check vertical victory
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
+                return true;
+        }
+        if((board[0][0] == board[1][1] && board[0][0] == board[2][2]) || (board[0][2] == board[1][1] && board[0][0] == board[2][0]))
+            return true;
+        return false;
+    }
 
-		}
-		for (int i = 0; i < board[0].length; i++) {// check vertical victory
+    public void makeMove(int x, int y, int team) {
+        if ((team == 1 || team == 2) && board[y][x] == 0)
+            board[y][x] = team;
+    }
 
-		}
-		return false;
-	}
-
-	public void makeMove(int x, int y, int team) {
-		if ((team == 1 || team == 2) && board[y][x] == 0)
-			board[y][x] = team;
-	}
-
-	public void resetBoard() {
-		for (int x = 0; x < board.length; x++) {
-			for (int y = 0; y < board[x].length; y++) {
-				board[y][x] = 0;
-			}
-		}
-	}
-	
-	public toString() {// This needs to display the board IDK how. Graphics? Special Print way?  
-		
-	}
+    public void resetBoard() {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
+                board[y][x] = 0;
+            }
+        }
+    }
+    
+    public String toString() {// This needs to display the board IDK how. Graphics? Special Print way?  
+        return board.toString();
+    }
 }
