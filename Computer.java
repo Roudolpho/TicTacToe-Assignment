eede7e12e56abfda8b417f1c922d4ed2e8ad1855
cpham1986 <chrispham0900@yahoo.com>
2018-01-31 12:52:51 -0500

    some work to computer

--- Computer.java
+++ Computer.java
@@ -1,4 +1,4 @@
-package src;
+ 
 
 /**
  * Write a description of class computer here.
@@ -6,9 +6,9 @@ package src;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Computer
+public class Computer extends Player
 {
-    int row, col, team, o;
+    int row, col, team, o, spot;
     public Computer(int t){
         team = t;
     }
@@ -34,14 +34,22 @@ public class Computer
 
     /*should check for any possible crosses though columns*/
     public void checkColumns(int[][] asd){
+        o = 0;
         for(int i = 0; i<=2;i++)
-            for(int c = 0;c<=1;c++)
-                if(asd[i][c] != 0 && asd[i][c+1] != 0)
-                    for(int x=0;x<=2;x++)
-                        if(asd[i][x] == 0){
-                            row = i;
-                            col = x;
-                        }
+            for(int c = 0;c<=2;c++){
+                if(asd[i][c] == 1)
+                    o += 1;
+                if(o == 2){
+                    o = i;
+                    c = 2;
+                    i = 2;
+                }
+            }
+        for(int x=0;x<=2;x++)
+            if(asd[o][x] == 0){
+                row = o;
+                col = x;
+            }
     }
 
     /*should check for any possible crosses though rows*/
@@ -52,14 +60,31 @@ public class Computer
                 if(asd[i][c] == 1)
                     o += 1;
                 if(o == 2){
-                    
+                    o = c;
+                    c = 2;
+                    i = 2;
                 }
             }
         for(int x=0;x<=2;x++)
-                        if(asd[x][c] == 0){
-                            row = x;
-                            col = c;
-                        }
+            if(asd[x][o] == 0){
+                row = x;
+                col = o;
+            }
+    }
+    
+    public void checkDiagonals(int[][] asd){
+        o = 0;
+        spot = 0;
+        for(int c = 0; c<=2;c++)
+            for(int i = 2;i>=0;i++){
+                if(asd[i][c] == 1){
+                    o += 1;
+                } else if(asd[i][c] == 0)
+                if(o == 2){
+                    
+                }
+            }
+        
     }
 
     public int getX(){
