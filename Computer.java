fdab72d89a88beb9aa3bca9d756badaef20c0dbf
cpham1986 <chrispham0900@yahoo.com>
2018-02-01 12:39:38 -0500

    computer changes
    made changes to the computer class

--- Computer.java
+++ Computer.java
@@ -7,51 +7,60 @@
  */
 public class Computer extends Player
 {
-    int row, col, team, o, spot;
+    int row, col, team, o, spot, l;
+    int[][] b = null;
     public Computer(int t, Board board1, int level){
         team = t;
         if (t == 1)
-			teamstr = "one";
-		if (t == 2)
-			teamstr = "two";
-		board = board1;
+            teamstr = "one";
+        if (t == 2)
+            teamstr = "two";
+        board = board1;
+        b = board.getBoard();
+        l = level;
     }
+
     @Override public void TakeTurn() {
-    	//this should check for if computer is set to easy or hard and then takes a turn. 
+        //this should check for if computer is set to easy or hard and then takes a turn. 
+        switch(l)
+        {
+            case 1: getSpaceEasy();
+            case 2: getSpaceHard();
+        }
     }
-    public void getSpaceEasy(int[][] asd){ //I don't think that you need to call the board each time because in board.makemove it uses the board itself. 
+
+    public void getSpaceEasy(){ //I don't think that you need to call the board each time because in board.makemove it uses the board itself. 
         row = -1;
         col = -1;
         do{
             row = (int)(3*Math.random());
             col = (int)(3*Math.random());    
-        } while(asd[row][col]!=0); //board.makemove() now returns a boolean if it makes a move for you so you can use board.makemove to both check if a move is possible and make the move at the same time. 
+        } while(board.makeMove(row,col,team) == false); //board.makemove() now returns a boolean if it makes a move for you so you can use board.makemove to both check if a move is possible and make the move at the same time. 
     }
 
-    public void getSpaceHard(int[][] asd){
+    public void getSpaceHard(){
         row = -1;
         col = -1;
-        checkRows(asd);
-        if(row == -1 && col == -1)
-            checkColumns(asd);
-        if(row == -1 && col == -1)
-            checkDiagonals(asd);
-        if(row == -1 && col == -1)
-            checkDiagonals2(asd);
-        if(row == -1 && col == -1)
-            getSpaceEasy(asd);
-        
+        checkRows();
+        if(board.makeMove(row,col,team) == false)
+            checkColumns();
+        if(board.makeMove(row,col,team) == false)
+            checkDiagonals();
+        if(board.makeMove(row,col,team) == false)
+            checkDiagonals2();
+        if(board.makeMove(row,col,team) == false)
+            getSpaceEasy();
     }
 
     /*should check for any possible crosses through columns*/
-    public void checkColumns(int[][] asd){
+    public void checkColumns(){
         o = 0;
         spot = -1;
         for(int i = 0; i<=2;i++)
             for(int c = 0;c<=2;c++){
-                if(asd[i][c] == 1){
+                if(b[i][c] == 1){
                     o += 1;
-                } else if(asd[i][c] == 0)
+                } else if(b[i][c] == 0)
                     spot = c;
                 if(o == 2 && spot != -1){
                     row = i;
@@ -63,14 +72,14 @@ public class Computer extends Player
     }
 
     /*should check for any possible crosses through rows*/
-    public void checkRows(int[][] asd){
+    public void checkRows(){
         o = 0;
         spot = -1;
         for(int c = 0; c<=2;c++)
             for(int i = 0;i<=2;i++){
-                if(asd[i][c] == 1){
+                if(b[i][c] == 1){
                     o += 1;
-                } else if(asd[i][c] == 0)
+                } else if(b[i][c] == 0)
                     spot = c;
                 if(o == 2 && spot != -1){
                     row = spot;
@@ -81,14 +90,14 @@ public class Computer extends Player
             }
     }
 
-    public void checkDiagonals(int[][] asd){
+    public void checkDiagonals(){
         o = 0;
         spot = -1;
         for(int c = 0; c<=2;c++)
             for(int i = 2;i>=0;i++){
-                if(asd[i][c] == 1){
+                if(b[i][c] == 1){
                     o += 1;
-                } else if(asd[i][c] == 0)
+                } else if(b[i][c] == 0)
                     spot = i;
                 if(o == 2 && spot != -1){
                     row = i;
@@ -98,15 +107,15 @@ public class Computer extends Player
                 }
             }
     }
-    
-     public void checkDiagonals2(int[][] asd){
+
+    public void checkDiagonals2(){
         o = 0;
         spot = -1;
         for(int i = 0; i<=2;i++)
             for(int c = 2;c>=0;c++){
-                if(asd[i][c] == 1){
+                if(b[i][c] == 1){
                     o += 1;
-                } else if(asd[i][c] == 0)
+                } else if(b[i][c] == 0)
                     spot = i;
                 if(o == 2 && spot != -1){
                     row = i;
