540faeb5fc219a6eff80eb28aa649b3526abcd38
Roudolpho <roudolphigalous@yahoo.com>
2018-02-01 12:41:16 -0500

    d
    d

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,49 +8,43 @@
 import java.util.*;
 
 public class TicTacToe {
-	public static void main(String args[]) {
-		Scanner scan = new Scanner(System.in);
-		Visuals screen = new Visuals();
-		Board board = new Board();
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        Visuals screen = new Visuals();
+        Board board = new Board();
 
-		screen.setUpTicTacToe();
+        screen.setUpTicTacToe();
 
-		System.out.println("Are you playing the computer \"yes\" or \"no\"");
-		String str = scan.nextLine();
+        System.out.println("Are you playing the computer \"yes\" or \"no\"");
+        String str = scan.nextLine();
 
-		Player player1 = new Player(1, board);
-		Player player2;
+        Player player1 = new Player(1, board);
+        Player player2;
 
-		if (str.toLowerCase().equals("yes")) {
-			System.out.println("What level of the computer ar you playeing easy(1) or hard(2)"); 
-			int lvl = scan.nextInt();
-			player2 = new Computer(2, board, lvl);
-		} else {
-			player2 = new Player(2, board);
-		}
-		boolean win = false;
-		int winningTeam = 0;
-		while (win == false) {// print board then do player1's turn then computer's or player2's
-			player1.TakeTurn();
-			if (board.checkVictory()) {
-				win = true;
-				winningTeam = 1;
-			}
-			player2.TakeTurn();
-			if (board.checkVictory()) {
-				win = true;
-				winningTeam = 2;
-			}
-		}
-<<<<<<< HEAD
-		
-		
-		
-=======
+        if (str.toLowerCase().equals("yes")) {
+            System.out.println("What level of the computer ar you playeing easy(1) or hard(2)"); 
+            int lvl = scan.nextInt();
+            player2 = new Computer(2, board, lvl);
+        } else {
+            player2 = new Player(2, board);
+        }
+        boolean win = false;
+        int winningTeam = 0;
+        while (win == false) {// print board then do player1's turn then computer's or player2's
+            player1.TakeTurn();
+            if (board.checkVictory()) {
+                win = true;
+                winningTeam = 1;
+            }
+            player2.TakeTurn();
+            if (board.checkVictory()) {
+                win = true;
+                winningTeam = 2;
+            }
+        }
 
-		System.out.println("Team " + winningTeam + " won");
-		scan.close();
->>>>>>> 646eed33b3a14c556ff2980f29e1c207c2e5810d
-	}
+        System.out.println("Team " + winningTeam + " won");
+        scan.close();
+    }
 }
 
