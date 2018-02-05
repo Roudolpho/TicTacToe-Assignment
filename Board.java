d6bd88d8a84774b523a0ef95f3f2f1b0d9095971
cpham1986 <chrispham0900@yahoo.com>
2018-02-04 23:53:27 -0500

    computer finished
    this probably works hopefully

--- Board.java
+++ Board.java
@@ -6,69 +6,69 @@
  * @version 0.0.0.1
  */
 public class Board {
-	private int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+    private int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-	/*
-	 * 0-Empty space 1-Represents X 2-Represents O
-	 */
-	/**
-	 * empty constructor
-	 *
-	 */
-	Board() {
+    /*
+     * 0-Empty space 1-Represents X 2-Represents O
+     */
+    /**
+     * empty constructor
+     *
+     */
+    Board() {
 
-	}
+    }
 
-	public int[][] getBoard() { // Method so that the main method may retrieve the state of the board
-		return board;
-	}
+    public int[][] getBoard() { // Method so that the main method may retrieve the state of the board
+        return board;
+    }
 
-	public boolean checkVictory() {
-		for (int i = 0; i < board.length; i++) {// check horizontal victory
-			if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 0)
-				return true;
-		}
-		for (int i = 0; i < board[0].length; i++) {// check vertical victory
-			if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 0)
-				return true;
-		}
-		if (((board[0][0] == board[1][1] && board[0][0] == board[2][2])
-				|| (board[0][2] == board[1][1] && board[0][0] == board[2][0])) && board[1][1] != 0)
-			return true;
-		return false;
-	}
+    public boolean checkVictory() {
+        for (int i = 0; i < board.length; i++) {// check horizontal victory
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 0)
+                return true;
+        }
+        for (int i = 0; i < board[0].length; i++) {// check vertical victory
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 0)
+                return true;
+        }
+        if (((board[0][0] == board[1][1] && board[0][0] == board[2][2])
+            || (board[0][2] == board[1][1] && board[0][0] == board[2][0])) && board[1][1] != 0)
+            return true;
+        return false;
+    }
 
-	public boolean makeMove(int x, int y, int team) {
-		if(x == -1 || y == -1)
-			return false;
-		if ((team == 1 || team == 2) && board[y][x] == 0) {
-			board[y][x] = team;
-			return true;
-		}
-		return false;
-	}
+    public boolean makeMove(int x, int y, int team) {
+        if(x == -1 || y == -1)
+            return false;
+        if ((team == 1 || team == 2) && board[y][x] == 0) {
+            board[y][x] = team;
+            return true;
+        }
+        return false;
+    }
 
-	public boolean checkMove(int x, int y) {
-		if (board[y][x] == 0)
-			return true;
-		return false;
-	}
+    public boolean checkMove(int x, int y) {
+        if (board[y][x] == 0)
+            return true;
+        return false;
+    }
 
-	public int getSpace(int x, int y) {
-		return board[y][x];
-	}
+    public int getSpace(int x, int y) {
+        return board[y][x];
+    }
 
-	public void resetBoard() {// resets the board
-		for (int x = 0; x < board.length; x++) {
-			for (int y = 0; y < board[x].length; y++) {
-				board[y][x] = 0;
-			}
-		}
-	}
+    public void resetBoard() {// resets the board
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
+                board[y][x] = 0;
+            }
+        }
+    }
 
-	public String toString() {// This needs to display the board IDK how. Graphics? Special Print way?
-		String temp = "\t" + board[0][0] + "\t" + board[0][1] + "\t" + board[0][2] + "\t\n\t" + board[1][0] + "\t"
-				+ board[1][1] + "\t" + board[1][2] + "\t\n\t" + board[2][0] + "\t" + board[2][1] + "\t" + board[2][2] + "\n";
-		return temp;
-	}
+    public String toString() {// This needs to display the board IDK how. Graphics? Special Print way?
+        String temp = "\t" + board[0][0] + "\t" + board[0][1] + "\t" + board[0][2] + "\t\n\t" + board[1][0] + "\t"
+            + board[1][1] + "\t" + board[1][2] + "\t\n\t" + board[2][0] + "\t" + board[2][1] + "\t" + board[2][2] + "\n";
+        return temp;
+    }
 }
--- Computer.java
+++ Computer.java
@@ -6,144 +6,163 @@
  * @version (a version number or a date)
  */
 public class Computer extends Player {
-	int row, col, team, o, spot, l;
+    int row, col, team, o, spot, l, v;
 
-	public Computer(int t, Board board1, int level) {
-		team = t;
-		if (t == 1)
-			teamstr = "one";
-		if (t == 2)
-			teamstr = "two";
-		board = board1;
-		l = level;
-		comp = true;
-	}
+    public Computer(int t, Board board1, int level) {
+        team = t;
+        if (t == 1)
+            teamstr = "one";
+        if (t == 2)
+            teamstr = "two";
+        board = board1;
+        l = level;
+        comp = true;
+    }
 
-	@Override
-	public void TakeTurn() {
-		// this should check for if computer is set to easy or hard and then takes a
-		// turn.
-		System.out.println(board.toString());
-		System.out.println("Computer is taking its turn...");
-		switch (l) {
-		case 1:
-			getSpaceEasy();
-			break;
-		case 2:
-			getSpaceHard();
-			break;
-		}
-	}
+    @Override
+    public void TakeTurn() {
+        // this should check for if computer is set to easy or hard and then takes a
+        // turn.
+        System.out.println(board.toString());
+        System.out.println("Computer is taking its turn...");
+        switch (l) {
+            case 1:
+            getSpaceEasy();
+            break;
+            case 2:
+            getSpaceHard();
+            break;
+        }
+    }
 
-	public void getSpaceEasy() { // I don't think that you need to call the board each time because in
-									// board.makemove it uses the board itself.
-		do {
-			row = (int) (2 * Math.random());
-			col = (int) (2 * Math.random());
-		} while (board.makeMove(row, col, team) == false); // board.makemove() now returns a boolean if it makes a move
-															// for you so you can use board.makemove to both check if a
-															// move is possible and make the move at the same time.
-	}
+    public void getSpaceEasy() { // I don't think that you need to call the board each time because in
+        // board.makemove it uses the board itself.
+        do {
+            row = (int) (3 * Math.random());
+            col = (int) (3 * Math.random());
+        } while (board.makeMove(row, col, team) == false); // board.makemove() now returns a boolean if it makes a move
+        // for you so you can use board.makemove to both check if a
+        // move is possible and make the move at the same time.
+    }
 
-	public void getSpaceHard() {
-		checkRows();
-		if (board.makeMove(row, col, team) == false) {
-			checkColumns();
-			System.out.println("1");
-		}
-		if (board.makeMove(row, col, team) == false) {
-			checkDiagonals();
-			System.out.println("2");
-		}
-		if (board.makeMove(row, col, team) == false) {
-			checkDiagonals2();
-			System.out.println("3");
-		} else
-			getSpaceEasy();
-	}
+    public void getSpaceHard() {
+        row = -1;
+        col = -1;
+        checkRows();
+        if (row  == -1) {
+            checkColumns();
+            System.out.println("1");
+            System.out.println(board.toString());
+        }
+        if (row  == -1) {
+            checkDiagonals();
+            System.out.println("2");
+            System.out.println(board.toString());
+        }
+        if (row  == -1) {
+            checkDiagonals2();
+            System.out.println("3");
+            System.out.println(board.toString());
+        }
+        if(row  == -1)
+            getSpaceEasy();
+        System.out.print(col + " " + row);
+    }
 
-	/* should check for any possible crosses through columns */
-	public void checkColumns() {
-		o = 0;
-		spot = -1;
-		for (int i = 0; i <= 2; i++)
-			for (int c = 0; c <= 2; c++) {
-				if (board.getBoard()[i][c] == 1) {
-					o += 1;
-				} else if (board.getBoard()[i][c] == 0)
-					spot = c;
-				if (o == 2 && spot != -1) {
-					row = i;
-					col = spot;
-					c = 2;
-					i = 2;
-				}
-			}
-	}
+    /* should check for any possible crosses through columns */
+    public void checkColumns() {
+        for (int i = 0; i <= 2; i++){
+            o = 0;
+            spot = -1;
+            for (int c = 0; c <= 2; c++) {
+                if (board.getBoard()[i][c] == 1) {
+                    o += 1;
+                }else if (board.getBoard()[i][c] == 0)
+                    spot = c;
+                if (o == 2 && spot != -1) {
+                    row = i;
+                    col = spot;
+                    c = 3;
+                    i = 3;
+                    board.makeMove(col, row, team);
+                }
+            }
+        }
+    }
 
-	/* should check for any possible crosses through rows */
-	public void checkRows() {
-		o = 0;
-		spot = -1;
-		for (int c = 0; c <= 2; c++)
-			for (int i = 0; i <= 2; i++) {
-				if (board.getBoard()[i][c] == 1) {
-					o += 1;
-				} else if (board.getBoard()[i][c] == 0)
-					spot = i;
-				if (o == 2 && spot != -1) {
-					row = spot;
-					col = c;
-					c = 2;
-					i = 2;
-				}
-			}
-		spot = -1;
-	}
+    /* should check for any possible crosses through rows */
+    public void checkRows() {
+        for (int c = 0; c <= 2; c++){
+            o = 0;
+            spot = -1;      
+            for (int i = 0; i <= 2; i++) {
+                if (board.getBoard()[i][c] == 1 ) {
+                    o += 1;
+                }else if (board.getBoard()[i][c] == 0)
+                    spot = i;
+                if (o == 2 && spot != -1) {
+                    row = spot;
+                    col = c;
+                    System.out.println(i + " " + spot);
+                    i = 3;
+                    c = 3;
+                    board.makeMove(col, row, team);
+                }
+            }         
+        }
 
-	public void checkDiagonals() {
-		o = 0;
-		spot = -1;
-		for (int c = 0; c <= 2; c++)
-			for (int i = 2; i >= 0; i--) {
-				if (board.getBoard()[i][c] == 1) {
-					o += 1;
-				} else if (board.getBoard()[i][c] == 0)
-					spot = i;
-				if (o == 2 && spot != -1) {
-					row = i;
-					col = c;
-					c = 2;
-					i = 0;
-				}
-			}
-		spot = -1;
-	}
+    }
 
-	public void checkDiagonals2() {
-		o = 0;
-		spot = -1;
-		for (int i = 0; i <= 2; i++)
-			for (int c = 2; c >= 0; c--) {
-				if (board.getBoard()[i][c] == 1) {
-					o += 1;
-				} else if (board.getBoard()[i][c] == 0)
-					spot = i;
-				if (o == 2 && spot != -1) {
-					row = i;
-					col = c;
-					c = 0;
-					i = 2;
-				}
-			}
-		spot = -1;
-	}
+    public void checkDiagonals() {
+        o = 0;
+        spot = -1;
+        for (int i = 2; i >= 0; i--) {
+            if (board.getBoard()[i][i] == 1) {
+                o += 1;
+            }else if (board.getBoard()[i][i] == 0){
+                spot = i;
+            }
+            if (o == 2 && spot != -1) {
+                row = spot;
+                col = spot;
+                board.makeMove(col, row, team);
+            }
+        }
+    }
 
-	public int getX() {
-		return row;
-	}
+    public void checkDiagonals2() {
+        o = 0;
+        spot = -1;
+        for (int i = 2; i >= 0; i--) {
+            int c = 0;
+            switch(i)
+            {
+                case 0: c = 2;break;
+                case 1: c = 1;break;
+                case 2: c = 0;break;
+            }
+            if (board.getBoard()[i][c] == 1) {
+                o += 1;
+            }else if (board.getBoard()[i][c] == 0)
+                spot = i;
+            if (o == 2 && spot != -1) {
+                row = spot;
+                switch(spot)
+                {
+                    case 0: col = 2;break;
+                    case 1: col = 1;break;
+                    case 2: col = 0;break;
+                }
+                board.makeMove(col, row, team);
+            }
+        }
+    }
 
-	public int getY() {
-		return col;
-	}
+    public int getX() {
+        return row;
+    }
+
+    public int getY() {
+        return col;
+    }
 }
